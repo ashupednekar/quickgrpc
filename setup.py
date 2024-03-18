9582034219106667ee820c7e73c502c67728975e
ashupednekar <ashutoshpednekar15@gmail.com>
2024-03-18 22:04:53 +0530

    chore: added package

--- setup.py
+++ setup.py
@@ -29,30 +29,6 @@ def get_external_reqs():
         return [x for x in f.read().split("\n") if x]
 
 
-def get_internal_reqs():
-    """
-    The get_internal_reqs function is used to get the internal requirements for the project.
-    This function will read from a file called &quot;internal-requirements.txt&quot; and parse it into a list of strings that can be passed to pip install as dependencies.
-
-    :return: A list of dependencies
-    """
-    token = os.getenv("GIT_TOKEN")
-    if not token:
-        raise ImportError(
-            "Please make sure you set an env called GIT_TOKEN. \n $export GIT_TOKEN=<your token>"
-        )
-    deps = []
-    with open("internal-requirements.txt", "r") as f:
-        for req in f.read().split("\n"):
-            if not req:
-                continue
-            pkg, ver = req.split("==")
-            deps.append(
-                f"{pkg} @ git+https://{os.getenv('GIT_TOKEN')}@github.com/BankBuddy/bud-core-{pkg}-lib@{ver}"
-            )
-        return deps
-
-
 # This call to setup() does all the work
 setup(
     name="boilerplate",
@@ -60,13 +36,11 @@ setup(
     description="boilerplate library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://affinsys.com/",
+    url="https://github.com/ashupednekar",
     author="Ashutosh Pednekar",
-    author_email="ashutosh.pednekar@affinsys.com",
-    license="Proprietary",
+    author_email="ashupednekar49@gmail.com",
+    license="",
     classifiers=[
-        "Intended Audience :: Affinsys Developers",
-        "License :: Proprietary",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",

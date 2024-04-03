fbc75ef505573aad9c8211a273fd82b6b47b50cd
ashupednekar <ashutoshpednekar15@gmail.com>
2024-04-04 02:50:56 +0530

    fix: import issue

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_dependencies():
 # This call to setup() does all the work
 setup(
     name="quickgrpc",
-    version="0.0.6",
+    version="0.0.7",
     description="quickgrpc lib library",
     long_description=long_description,
     long_description_content_type="text/markdown",

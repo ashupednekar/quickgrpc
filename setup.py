aa1516de64a635dd0cad60ebe76b0e53061adffc
Ashutosh Pednekar <ashutoshpednekar15@gmail.com>
2024-07-10 18:10:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_dependencies():
 # This call to setup() does all the work
 setup(
     name="quickgrpc",
-    version="0.1.2",
+    version="0.1.3",
     description="quickgrpc lib library",
     long_description=long_description,
     long_description_content_type="text/markdown",
